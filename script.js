38913a9b464fdb80a1e4a1615058e6a2447ecdb6
YaAn1314 <S10267801@connect.np.edu.sg>
2024-11-27 19:11:24 +0800

    Added overlay effects to make page darken when sidebar is opened. Also improved the appearance of sidebar

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function openNav() {
-    // Make the sidebar visible
-    document.getElementById("sidebar").style.display = "block";
-    document.getElementById("sidebar").style.width = "250px"; // Slide-in effect
+    document.getElementById("sidebar").style.width = "250px"; // Adjust width as needed
+    document.getElementById("overlay").style.display = "block"; // Show the overlay
 }
 
 function closeNav() {
-    // Hide the sidebar
-    document.getElementById("sidebar").style.display = "none";
-    document.getElementById("sidebar").style.width = "0"; // Slide-out effect
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("overlay").style.display = "none"; // Hide the overlay
 }
