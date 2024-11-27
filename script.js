73ab8fcf16eb59bad3da2926c98fbb8e2d562c1c
YaAn1314 <S10267801@connect.np.edu.sg>
2024-11-27 17:55:32 +0800

    made sidebar clickable

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    // Make the sidebar visible
+    document.getElementById("sidebar").style.display = "block";
+    document.getElementById("sidebar").style.width = "250px"; // Slide-in effect
+}
+
+function closeNav() {
+    // Hide the sidebar
+    document.getElementById("sidebar").style.display = "none";
+    document.getElementById("sidebar").style.width = "0"; // Slide-out effect
+}
